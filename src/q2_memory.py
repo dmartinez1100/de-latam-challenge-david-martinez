66c1be2cdb6c5c35eab20c94a758d929d8a5b513
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 17:50:00 -0500

    approximation challenge 2

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,14 @@
 from typing import List, Tuple
+from collections import Counter
+import json
+import emoji
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+    n_emos = Counter()
+    with open(file_path, "r") as tweets_file:
+        for tweet in tweets_file:
+            a =  json.loads(tweet)
+            n_emos.update(emo["emoji"] for emo in emoji.emoji_list(a["content"]))
+
+        return n_emos.most_common(20)
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,32 @@
 from typing import List, Tuple
+from collections import Counter
+import json
+import emoji
+from concurrent.futures import ThreadPoolExecutor
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    
+    def countemojis(tws):
+        n_emos = Counter()
+        for tweet in tws:
+            tw_content =  json.loads(tweet)["content"]
+            n_emos.update(emo["emoji"] for emo in emoji.emoji_list(tw_content))
+        return n_emos
+
+    n_emos = Counter()
+    with open(file_path, "r") as tweets_file:
+        tweets = tweets_file.readlines()
+        total_lines = len(tweets)
+        lines_per_part = total_lines // 5
+
+        # Procesar las partes en paralelo
+        with ThreadPoolExecutor() as executor:
+            # Mapear la función de procesamiento a cada parte del archivo
+            resultados = list(executor.map(countemojis, [
+                tweets[(i * lines_per_part): ((i + 1) * lines_per_part if i < 4 else total_lines)] for i in range(5)
+            ]))
+        
+        for resultado in resultados:
+            n_emos.update(resultado)
+
+        return n_emos.most_common(20)
