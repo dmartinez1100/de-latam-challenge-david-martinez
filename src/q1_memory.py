49a4919cd4b18546e7649ca988b6318716ef76fd
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 16:48:40 -0500

    first approximation to the problem

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,16 @@
 from typing import List, Tuple
 from datetime import datetime
+from collections import defaultdict
+import json
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    count_dates_users = defaultdict(lambda : defaultdict(int))
+    with open(file_path, "r") as tweets_file:
+        for tweet in tweets_file:
+            a =  json.loads(tweet)
+            count_dates_users[a["date"].split("T")[0]][a["user"]["username"]] += 1
+        tweets_file.close()
+        
+        top_dates = sorted(count_dates_users.items(), key=lambda date_count: sum(date_count[1].values()))[-10:]
+        top_dates_user = list(map( lambda dt: (datetime.fromisoformat(dt[0]), max(count_dates_users[dt[0]].items(), key=lambda user: user[1])[0]), top_dates))
+        return sorted(top_dates_user)
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,15 @@
 from typing import List, Tuple
 from datetime import datetime
-
+from collections import defaultdict, Counter
+import json
+    
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    n_dates = Counter()
+    n_dates_2 = defaultdict(Counter)
+    with open(file_path, "r") as tweets_file:
+        for tweet in tweets_file:
+            a =  json.loads(tweet)
+            n_dates.update([a["date"].split("T")[0]])
+            n_dates_2[a["date"].split("T")[0]].update( [a["user"]["username"]] )
+        tweets_file.close()
+        return sorted([(datetime.fromisoformat(f[0]), n_dates_2[f[0]].most_common(1)[0][0]) for f in n_dates.most_common(10) ])
