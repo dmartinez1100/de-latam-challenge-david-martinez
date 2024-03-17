94ac2eb9e653241d688853f33b40e05b54086180
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 19:58:32 -0500

    fix to prehook

--- .git-hooks/pre-push.sh
+++ .git-hooks/pre-push.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-# @rationale: Ejecuta pre-validaciones, evita subir código basura.
-python3 pycodestyle src/
-python3 pylint src/
+# @rationale: Ejecuta pre-validaciones, evita subir código en mal estado.
+
+CURRENT_BRANCH=$(git symbolic-ref --short head)
+pycodestyle src/
+STYLE_STATUS=$?
+pylint src/
+PYLINT_STATUS=$?
+
+if ([ STYLE_STATUS -eq 0 ] && [ PYLINT_STATUS -eq 0 ]) || [[ "${CURRENT_BRANCH}" == *"style"* ]];
+then
+    echo "Controles pasados, se puede hacer push to ${CURRENT_BRANCH}"
+    exit 0
+else
+    echo "Hay errores de estilo de código, no se puede hacer push to ${CURRENT_BRANCH} -- pep8 -> ${STYLE_STATUS} -- pylint -> ${PYLINT_STATUS}"
+    exit 1
+fi
