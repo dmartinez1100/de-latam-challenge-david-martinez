37b8820d23830a46a1a808e982be513df5a09bf9
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 20:50:19 -0500

    fix codestyle q3

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,16 +1,23 @@
+# pylint: disable=W1514
+"""Module providing a function for challenge Q3 focused on time."""
 from typing import List, Tuple
 from collections import Counter
 import re
 import json
 from concurrent.futures import ThreadPoolExecutor
 
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    
-    def countemojis(tws):
+    """Function with code of Q3 focused on time."""
+    def count_users(tws):
         n_users = Counter()
         for tweet in tws:
-            tw_content =  json.loads(tweet)["content"]
-            n_users.update(user.replace("@", "").upper() for user in re.findall(r'@\w{4,15}\b', tw_content))
+            tw_content = json.loads(tweet)["content"]
+            n_users.update(
+                user.replace("@", "").upper()
+                for user
+                in re.findall(r'@\w{4,15}\b', tw_content)
+                )
 
         return n_users
 
@@ -18,16 +25,22 @@ def q3_time(file_path: str) -> List[Tuple[str, int]]:
     n_threads = 3
     with open(file_path, "r") as tweets_file:
         tweets = tweets_file.readlines()
-        total_lines = len(tweets)
-        lines_per_part = total_lines // n_threads
+        ln_count = len(tweets)
+        chunk_sz = ln_count // n_threads
+        chunks = [
+            tweets[(i * chunk_sz): ((i + 1) * chunk_sz if i < 4 else ln_count)]
+            for i
+            in range(5)
+            ]
 
         # Procesar las partes en paralelo
         with ThreadPoolExecutor() as executor:
             # Mapear la función de procesamiento a cada parte del archivo
-            resultados = list(executor.map(countemojis, [
-                tweets[(i * lines_per_part): ((i + 1) * lines_per_part if i < n_threads-1 else total_lines)] for i in range(n_threads)
-            ]))
-        
+            resultados = list(executor.map(
+                count_users,
+                chunks
+                ))
+
         for resultado in resultados:
             n_users.update(resultado)
 
