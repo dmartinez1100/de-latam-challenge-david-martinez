e9ad3927abe33801cd04537cebd79d9b68ec7852
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 20:25:28 -0500

    fix codestyle

--- src/q1_time.py
+++ src/q1_time.py
@@ -1,15 +1,26 @@
+# pylint: disable=W1514
+"""Module providing a function for challenge Q1 focused on time."""
 from typing import List, Tuple
 from datetime import datetime
 from collections import defaultdict, Counter
 import json
-    
+
+
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    n_dates = Counter()
-    n_dates_2 = defaultdict(Counter)
+    """Function with code of Q1 focused on time."""
+    counter_dates = Counter()
+    counter_dates_user = defaultdict(Counter)
     with open(file_path, "r") as tweets_file:
         for tweet in tweets_file:
-            a =  json.loads(tweet)
-            n_dates.update([a["date"].split("T")[0]])
-            n_dates_2[a["date"].split("T")[0]].update( [a["user"]["username"]] )
+            tweet = json.loads(tweet)
+            _date = tweet["date"].split("T")[0]
+            _user = tweet["user"]["username"]
+            counter_dates.update(_date)
+            counter_dates_user[_date].update([_user])
         tweets_file.close()
-        return sorted([(datetime.fromisoformat(f[0]), n_dates_2[f[0]].most_common(1)[0][0]) for f in n_dates.most_common(10) ])
+        return sorted([
+            (datetime.fromisoformat(f[0]),
+             counter_dates_user[f[0]].most_common(1)[0][0]
+             )
+            for f in counter_dates.most_common(10)
+            ])
