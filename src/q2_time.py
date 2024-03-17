50d6768661a1729ae4bd653dc6929a1de32327fe
dvdmartinez1100 <davidfel.martinez@urosario.edu.co>
2024-03-16 20:40:34 -0500

    fix style of code

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,32 +1,41 @@
+# pylint: disable=W1514
+"""Module providing a function for challenge Q2 focused on time."""
+from concurrent.futures import ThreadPoolExecutor
 from typing import List, Tuple
 from collections import Counter
 import json
 import emoji
-from concurrent.futures import ThreadPoolExecutor
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    
+    """Function with code of Q2 focused on time."""
     def countemojis(tws):
         n_emos = Counter()
         for tweet in tws:
-            tw_content =  json.loads(tweet)["content"]
+            tw_content = json.loads(tweet)["content"]
             n_emos.update(emo["emoji"] for emo in emoji.emoji_list(tw_content))
         return n_emos
 
     n_emos = Counter()
     with open(file_path, "r") as tweets_file:
         tweets = tweets_file.readlines()
-        total_lines = len(tweets)
-        lines_per_part = total_lines // 5
+        ln_count = len(tweets)
+        chunk_sz = ln_count // 5
+        chunks = [
+            tweets[(i * chunk_sz): ((i + 1) * chunk_sz if i < 4 else ln_count)]
+            for i
+            in range(5)
+            ]
 
         # Procesar las partes en paralelo
         with ThreadPoolExecutor() as executor:
             # Mapear la función de procesamiento a cada parte del archivo
-            resultados = list(executor.map(countemojis, [
-                tweets[(i * lines_per_part): ((i + 1) * lines_per_part if i < 4 else total_lines)] for i in range(5)
-            ]))
-        
+            resultados = list(executor.map(
+                countemojis,
+                chunks
+                ))
+
         for resultado in resultados:
             n_emos.update(resultado)
 
-        return n_emos.most_common(20)
+        return n_emos.most_common(10)
